ce9b1650bf68cbc225f78c3aa56b106f98319ee9
jbian <1286308434@qq.com>
2025-05-24 16:03:31 -0400

    log path

--- src/flask/Utils/Logger.py
+++ src/flask/Utils/Logger.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 from .Env import config
 
@@ -8,7 +9,18 @@ LOG_FORMAT = "%(asctime)s-%(levelname)s-%(filename)s:%(lineno)d ===%(message)s"
 
 def init_log():
     logging.basicConfig(level=config.log_level)
-    handler = RotatingFileHandler("../log/urchinArt.log", maxBytes=1024 * 1024 * 200, backupCount=10)
+
+    # 当前文件（Logger.py）所在目录
+    current_dir = os.path.dirname(__file__)
+    # 进入上级目录（即 flask/）
+    flask_dir = os.path.abspath(os.path.join(current_dir, ".."))
+    # 构造 log 文件夹路径
+    log_dir = os.path.join(flask_dir, "log")
+    os.makedirs(log_dir, exist_ok=True)
+    # 最终日志文件路径
+    log_path = os.path.join(log_dir, "urchinArt.log")
+
+    handler = RotatingFileHandler(log_path, maxBytes=1024 * 1024 * 200, backupCount=10)
 
     formatter = logging.Formatter(fmt=LOG_FORMAT, datefmt=DATE_FORMAT)
     handler.setFormatter(formatter)
